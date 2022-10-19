8a107f5760783adddba6578bf237865b65051901
reivaxbird <xavier.mezanges@gmail.com>
2022-10-19 11:27:46 +0200

    Ajoutez des fichiers projet.

---
+++ MySyndic/Models/MySyndicContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MySyndic.Models
+{
+    public class MySyndicContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString: @"server=localhost;port=15000;database=MySyndic;uid=reivax;password=Xm01012022!!;",
+                new MySqlServerVersion(new Version(8, 0, 31)));
+        }
+
+        public DbSet<CoOwner> CoOwner { get; set; }
+    }
+}
