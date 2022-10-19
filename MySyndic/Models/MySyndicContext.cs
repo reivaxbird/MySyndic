f3ca28a29996a350d5e70cb01e161e03e23e7697
reivaxbird <xavier.mezanges@gmail.com>
2022-10-19 11:29:27 +0200

    clean code

--- MySyndic/Models/MySyndicContext.cs
+++ MySyndic/Models/MySyndicContext.cs
@@ -6,7 +6,7 @@ namespace MySyndic.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql(connectionString: @"server=localhost;port=15000;database=MySyndic;uid=reivax;password=Xm01012022!!;",
+            optionsBuilder.UseMySql(connectionString: @"server=localhost;port=15000;database=MySyndic;uid=reivax;password=azerty123;",
                 new MySqlServerVersion(new Version(8, 0, 31)));
         }
 
